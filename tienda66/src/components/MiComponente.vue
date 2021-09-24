6895411e71b7d25ec150703d986e6182f1024464
Natacha7 <jennynatacha@misena.edu.co>
2021-09-24 16:42:08 -0500

    Se trabaja primer archivo vue

---
+++ tienda66/src/components/MiComponente.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Bienvenido {{ cliente.nombre }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            cliente: { nombre: 'Natacha', apellido: 'Libreros' },
+            cantidad: 0
+        }
+    }
+}
+</script>
